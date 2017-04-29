18461f75d0fd28c3342fd1e9cb1e0121bb879427
LascarPaul <lascar.paul@yahoo.com>
2017-04-29 17:04:28 +0300

    init

---
+++ app/src/main/java/com/example/intel/musicalstructure/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.intel.musicalstructure;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageButton search = (ImageButton) findViewById(R.id.search_button);
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent searchIntent = new Intent(MainActivity.this, Music.class);
+
+
+                startActivity(searchIntent);
+            }
+        });
+
+        ImageButton settings = (ImageButton) findViewById(R.id.settings_button);
+
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent settingsIntent = new Intent(MainActivity.this, Settings.class);
+
+
+                startActivity(settingsIntent);
+            }
+        });
+
+        ImageButton songInfo = (ImageButton) findViewById(R.id.song_info_button);
+
+
+        songInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent settingsIntent = new Intent(MainActivity.this, SongInfo.class);
+
+
+                startActivity(settingsIntent);
+            }
+        });
+    }
+
+}
